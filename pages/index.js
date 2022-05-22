b544bdcb7c1a43f058d84ef33d52484ee40df250
Mini Bhati <minibhati93@gmail.com>
2022-05-22 14:02:07 +0530

    Header component ready

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import styles from '../styles/Home.module.css'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <main>
-      </main>
+      <main></main>
       <Footer />
-    </div>
-  )
+    </>
+  );
 }
